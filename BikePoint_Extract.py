329208310b146451b767d23bce446ea79b4d1b2a
Hannah Norfolk <hannahnor@hotmail.com>
2026-09-23 16:07:26 +0100

    Added while loop and status if statements

--- BikePoint_Extract.py
+++ BikePoint_Extract.py
@@ -3,6 +3,7 @@ import requests
 import os
 import json
 from datetime import datetime
+import time
 
 #define the variables that allow us to call the API
 url = 'https://api.tfl.gov.uk/BikePoint/'
@@ -15,20 +16,44 @@ os.makedirs(data_dir, exist_ok = True)
 timestamp = datetime.now().strftime('%Y-%m-%d %H-%M-%S')
 filename = f'{data_dir}/{timestamp}.json'
 
-#send the GET request to the API to see if it works
-response = requests.get(url)
+# set up retry settings in case the API fails
+max_retry = 5
+attempt = 0
+delay = 10
 
-# Get Status
-status = response.status_code
+# Keep trying until the maximum number of attempts is reached
 
-#Write an if statement based on the status code
+while attempt < max_retry:
+        
+    #send the GET request to the API to see if it works
+    response = requests.get(url)
 
-#Convert the JSON response into python variable
-data = response.json()
+    # Get Status
+    status = response.status_code
 
-# Open the output file and write the API data to it as JSON
-with open(filename, 'w') as file:
-    json.dump(data,file)
+    #Write an if statement based on the status code
+    if 200 <= status < 300:
+        #Convert the JSON response into python variable
+        data = response.json()
 
-#print the status code to see if the API call has worked. From the response, get the status code
-print(response.status_code)
+        # Open the output file and write the API data to it as JSON
+        with open(filename, 'w') as file:
+            json.dump(data,file)
+
+        # print the success comment, and break out the while loop
+        print(f'{filename} was successfully saved. Yipee!')
+        break
+
+    #Write the elif statement - for the server or client side errors
+    elif status < 200 or status >= 500:
+        time.sleep(delay)
+        attempt +=1
+        print(f'Status code:{status}.Retrying. Attempt number {attempt}')
+
+
+    else:
+        print(f'Error. Status code {status}. Fix it')
+        break
+
+    #print the status code to see if the API call has worked. From the response, get the status code
+    #    print(response.status_code)
